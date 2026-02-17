ac4351923deb98d89dcce488988f954fb38a2439
MightyOP-MAX <aman.raghav7862@gmail.com>
2026-02-17 13:49:02 +0530

    fv

---
+++ debugging_2_class.cpp
@@ -0,0 +1,26 @@
+/*Expected Output (After Fixing)
+1 occurs 2 times
+2 occurs 2 times
+3 occurs 1 time*/
+
+
+ //Frequency Counter (Avoid Duplicate Printing)
+#include<iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {1, 2, 2, 3, 1};
+
+    for(int i = 0; i < 5; i++) {
+        int count = 0;
+
+        for(int j = 0; j < 5; j++) {
+            if(arr[i] == arr[j])
+                count++;
+        }
+
+        cout << arr[i] << " occurs " << count << " times\n";
+    }
+
+    return 0;
+}
