d7dcab2e2c50e8801644411e8fff3a8095a9f908
MightyOP-MAX <aman.raghav7862@gmail.com>
2026-02-24 13:46:33 +0530

    ..

--- candy_class.cpp
+++ candy_class.cpp
@@ -8,5 +8,31 @@ The class should have the following member functions:
 •⁠  ⁠displayCandy(): Displays the color and points of the candy.
 In main(), create two Candy objects, set their values using setCandy method, and display them using displayCandy().*/
 
-
-
+#include<iostream>
+using namespace std;
+class Candy
+{
+    string color;
+    int points;
+    public:
+    void setCandy(string c,int p);
+    void displayCandy();
+};
+void Candy::setCandy(string c, int p)
+{
+    color=c; points=p;
+}
+void Candy::displayCandy()
+{
+    cout<<"\n Color of the Candy : "<<color;
+    cout<<"\n Your points are : "<<points;
+}
+int main()
+{
+    Candy c1,c2;
+    c1.setCandy("Blue",28);
+    c1.displayCandy();
+    c2.setCandy("Black",23);
+    c2.displayCandy();
+    return 0;
+}
