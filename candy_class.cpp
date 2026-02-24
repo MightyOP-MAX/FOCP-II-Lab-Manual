886f6610c35dd47a3484d0433acf299a0935fdbd
MightyOP-MAX <aman.raghav7862@gmail.com>
2026-02-24 13:41:25 +0530

    ..

---
+++ candy_class.cpp
@@ -0,0 +1,12 @@
+/*Question of the Day!!
+Design a C++ program that simulates a basic Candy Crush game element using class and object creation.
+Create a class Candy with the following attributes:
+•⁠  ⁠string color (stores the color of the candy)
+•⁠  ⁠int points (stores the points awarded)
+The class should have the following member functions:
+•⁠  ⁠setCandy(string c, int p): Sets the color and points of the candy.
+•⁠  ⁠displayCandy(): Displays the color and points of the candy.
+In main(), create two Candy objects, set their values using setCandy method, and display them using displayCandy().*/
+
+
+
