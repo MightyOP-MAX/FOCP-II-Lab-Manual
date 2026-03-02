f82463ce19f0abebab70175590d6af21cc4f21bc
MightyOP-MAX <aman.raghav7862@gmail.com>
2026-03-02 09:36:13 +0530

    sddfg

--- swap_function_code.cpp
+++ swap_function_code.cpp
@@ -1,33 +1,41 @@
 #include <iostream>
 using namespace std;
 
-void Swap (int a, int b);
-void Swap (float x, float y);
+void Swap (int &a, int &b);
+void Swap (float &x, float &y);
 
 int main()
 {int a,b;
-    cin>>a>>b;
+    cout<< "enter a";
+    cin>>a;
+
+    cout<<"enter b";
+    cin>>a;
 
     float x,y;
-    cin>>x>>y;
+     cout<< "enter x";
+    cin>>x;
+
+     cout<< "enter y";
+    cin>>y;
 
     Swap (a, b);  
     Swap (x, y); 
 
     cout<<"After Swap\n";
-    cout<< a << " " << b << "\n";   
-    cout<< x << " " << y << endl; 
+    cout<< a <<" "<< b << "\n";   
+    cout<< "enter x"<<x << " enter y"<< y << endl; 
     return 0;
 }
 
-void Swap(int a, int b){ 
+void Swap(int &a, int &b){ 
         int t = a;
         a = b;
         b = t;
         cout << a << " " << b << endl;
     }
 
-void Swap(float x, float y){ 
+void Swap(float &x, float &y){ 
         float t = x;
         x = y;
         y = t;
